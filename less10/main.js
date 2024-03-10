7d4fc4fc7fc7388d21449070622408907afc772d
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-03-11 00:33:13 +0200

    hw_10 part done

--- less10/main.js
+++ less10/main.js
@@ -17,12 +17,13 @@ firstForm.onsubmit = function (ev) {
 
 let numblock = document.getElementsByClassName('numberBlock')[0];
 document.onreadystatechange = function () {
-if (document.readyState === 'interactive') {
- let savenumb = JSON.parse(localStorage.getItem('numbr')) || parseFloat(numblock.outerText);
-    savenumb += 1;
-    numblock.innerText = savenumb;
-    localStorage.setItem('numbr', JSON.stringify(savenumb));
-};
+    if (document.readyState === 'interactive') {
+        let savenumb = JSON.parse(localStorage.getItem('numbr')) || parseFloat(numblock.outerText);
+        savenumb += 1;
+        numblock.innerText = savenumb;
+        localStorage.setItem('numbr', JSON.stringify(savenumb));
+    }
+    ;
 };
 // Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховще,
 //     в масив sessions зберігається інформація про дату та час відвідування сторінки.
@@ -44,11 +45,49 @@ localStorage.setItem('dateSesions', JSON.stringify(dateSesions));
 // При натисканні prev виводяться попередні 10 об'єктів
 
 
-let newarr = () => {
-    for (let i = 0; i < 101; i++) {
-         let arr = [];
-        arr.push(i);
+// let newarr = () => {
+//     let arr = [];
+//     for (let i = 0; i < 100; i++) {
+//         arr.push(i+1);
+//     };
+//     return arr
+// };
+let newarr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
+console.log(newarr);
+
+let listblock = document.getElementsByClassName('list')[0];
+let step = JSON.parse(localStorage.getItem('stepnumber')) || 0;
+console.log(step)
+
+let actualnumber = step*10;
+if (actualnumber < newarr.length && actualnumber >= 0) {
+    for (i = actualnumber; i < actualnumber + 10; i++) {
+        let listelement = document.createElement('h3')
+        listelement.innerText = newarr[i];
+        listblock.appendChild(listelement)
     };
-    return arr
-};
-console.log(newarr())`
+}
+
+
+let nextButton = document.getElementsByClassName('nextbutton')[0];
+let previosbutton = document.getElementsByClassName('previosbutton')[0];
+
+nextButton.onclick = function () {
+    location.reload();
+    step += 1;
+    step <= (newarr.length/10) ? localStorage.setItem('stepnumber', JSON.stringify(step)) : step = newarr.length/10;
+    localStorage.setItem('stepnumber', JSON.stringify(step))
+    };
+
+previosbutton.onclick = function () {
+    location.reload();
+    step -= 1;
+    step>=0 ? localStorage.setItem('stepnumber', JSON.stringify(step)) : step = 0;
+
+   };
+
+
+
+
+
+
