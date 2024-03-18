dcb6d2257522067a47ed6215f3f5ec792315a6b7
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-03-19 01:05:59 +0200

    make some changes in proj (clean)

--- MiniProject/main.js
+++ MiniProject/main.js
@@ -6,23 +6,12 @@ function createDom(text, classname, tag, appendPlace) {
     return this.element;
 }
 
-// let usersWrap = document.createElement('div');
-// usersWrap.classList.add('usersWrap');
-// document.body.appendChild(usersWrap)
-
 let usersWrap = new createDom('', 'usersWrap', 'div', document.body);
 
 // ----------------------------------------|audio Block|-------------------------------------------------------
 
-// let audioBlock = document.createElement('div');
-// audioBlock.classList.add('audioBlock');
-// document.body.appendChild(audioBlock);
-
 let audioBlock = new createDom('', 'audioBlock', 'div', document.body);
 
-
-// let audioText = document.createElement('h5');
-// audioText.innerText = 'Push to Play!';
 let audioText = new createDom('Push to Play!', 'audiotext', 'h5', audioBlock);
 
 let audioblokimg = document.createElement('img');
@@ -41,9 +30,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
     .then(users => {
 
         for (let user of users) {
-            // let userBlock = document.createElement('div');
-            // userBlock.classList.add('userBlock');
-            // usersWrap.appendChild(userBlock);
+
             let userBlock = new createDom('', 'userBlock', 'div', usersWrap)
 
             let mainInfouser = document.createElement('a');
